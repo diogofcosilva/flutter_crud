467707d55d705d7a3b2f43c06a0d1f45a2119b5e
Diogo Silva <diogosilva@prodata.inf.br>
2020-05-22 19:00:42 -0300

    criando crud em flutter

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png'),
+          '2': const User(
+      id: '2',
+      name: 'Jose',
+      email: 'jose@alunos.com.br',
+      avatarUrl:
+          '')
+};
