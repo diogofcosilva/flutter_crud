59900009d54eb5879f97a51e1858ac27046345f8
Diogo Silva <diogosilva@prodata.inf.br>
2020-05-26 13:07:33 -0300

    implementado create, alter e delete

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
@@ -22,12 +25,43 @@ class UserTile extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.edit),
               color: Colors.orange,
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(AppRoutes.USER_FORM, arguments: user);
+              },
             ),
             IconButton(
               icon: Icon(Icons.delete),
               color: Colors.red,
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Excluir Usuário'),
+                    content: Text('Tem certeza???'),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text('Não'),
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                      ),
+                      FlatButton(
+                        child: Text('Sim'),
+                        onPressed: () {
+                          //se não implementar o then do show dialog passando o parametro pelo pop, descomente a linha abaixo
+                          // Provider.of<Users>(context, listen: false).remove(user);
+                          Navigator.of(context).pop(true);
+                        },
+                      ),
+                    ],
+                  ),
+                ).then((confirmed) {
+                  if (confirmed) {
+                    Provider.of<Users>(context, listen: false).remove(user);
+                  }
+                });
+              },
             )
           ])),
     );
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/components/user_tile.dart';
 import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
 import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget {
@@ -23,6 +24,16 @@ class UserList extends StatelessWidget {
 
               // remover usuario
               // users.remove(users.byIndex(0));
+
+              //edição
+              // users.put(User(id: '2',
+              // name: 'Rafael',
+              // email: 'rafael@teste.com',
+              // avatarUrl: ''));
+
+              Navigator.of(context).pushNamed(
+                AppRoutes.USER_FORM
+              );
             },
           )
         ],
